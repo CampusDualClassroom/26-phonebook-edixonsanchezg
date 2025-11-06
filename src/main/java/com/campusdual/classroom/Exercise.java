b252e007e98c52ad1763f2601a5aeec383b472e9
Edixon Sánchez <ediiixonsanchezg@gmail.com>
2025-11-06 11:54:08 +0100

    ejercicio 26 terminado

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,7 +1,11 @@
 package com.campusdual.classroom;
 
+import java.util.Scanner;
+
 public class Exercise {
     public static void main(String[] args) {
-
+            Phonebook phonebook = new Phonebook();
+            phonebook.showMenu();
     }
 }
+
